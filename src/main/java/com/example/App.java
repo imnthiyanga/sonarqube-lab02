69acf4429eca42b128a8191abd2adc97da7cc563
ImanThiyanga <ithiyanga@gmail.com>
2026-02-02 09:30:11 +0530

    Introduce quality issues

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,15 +1,16 @@
-package main.java.com.example;
+package com.example;
 
 public class App {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception { 
 
-        Calculator calc = new Calculator();
+        Calculator calc = new Calculator(); 
 
-        System.out.println(calc.calculate(10, 5, "add"));
-
-        UserService service = new UserService();
-        service.findUser("admin");
+        System.out.println(calc.calculate(10, 5, "add-again"));
+         
+        UserService service = new UserService(); 
+        service.findUser("admin"); 
+        service.deleteUser("admin"); // NEW dangerous call 
     }
 }
 
