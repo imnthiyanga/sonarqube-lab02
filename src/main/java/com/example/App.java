9c419b22bfbb13651d569f729e042ff62682b895
ImanThiyanga <ithiyanga@gmail.com>
2026-02-02 09:10:41 +0530

    git commit -m "Introduce quality issues"

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,15 +1,9 @@
-package main.java.com.example;
+public static void main(String[] args) throws Exception { 
+    Calculator calc = new Calculator(); 
 
-public class App {
-
-    public static void main(String[] args) throws Exception {
-
-        Calculator calc = new Calculator();
-
-        System.out.println(calc.calculate(10, 5, "add"));
-
-        UserService service = new UserService();
-        service.findUser("admin");
-    }
-}
-
+    System.out.println(calc.calculate(10, 5, "add-again")); 
+    
+    UserService service = new UserService(); 
+    service.findUser("admin"); 
+    service.deleteUser("admin"); // NEW dangerous call 
+} 
